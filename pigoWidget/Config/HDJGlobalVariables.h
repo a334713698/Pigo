b968dcca43ea4a677439261fc8c06c5789b50472
hongdongjie <334713698@qq.com>
2019-01-04 13:47:40 +0800

    新增文件

---
+++ pigoWidget/Config/HDJGlobalVariables.h
@@ -0,0 +1,28 @@
+//
+//  HDJGlobalVariables.h
+//  iOSProjectFramework
+//
+//  Created by 洪冬介 on 2018/1/26.
+//  Copyright © 2018年 洪冬介. All rights reserved.
+//
+
+#ifndef HDJGlobalVariables_h
+#define HDJGlobalVariables_h
+
+/** 统一管理全局变量 */
+
+/** 是否是第一次打开 */
+#define IS_FIRSTOPEN                            @"IS_FIRSTOPEN"
+
+#define APP_SECRET @""
+
+#define DATABASE_NAME                     @"pigo_db.sqlite"
+#define task_list_table                   @"task_list_table"
+#define tomato_record_table               @"tomato_record_table"
+#define check_in_table                    @"check_in_table"
+
+#define Focuse_EndTimeStamp                    @"Focuse_EndTimeStamp"
+
+#define TaskListData                    @"TaskListData"
+
+#endif /* HDJGlobalVariables_h */
---
+++ pigoWidget/Model/PGTaskList/PGTaskListModel.h
@@ -0,0 +1,30 @@
+//
+//  PGTaskListModel.h
+//  iOSProjectFramework
+//
+//  Created by 洪冬介 on 2018/9/14.
+//  Copyright © 2018年 洪冬介. All rights reserved.
+//
+
+@interface PGTaskListModel : NSObject
+
+@property (nonatomic, assign) long add_time;
+@property (nonatomic, assign) long delete_time;
+
+@property (nonatomic, assign) NSInteger task_id;
+
+@property (nonatomic, copy) NSString *task_name;
+@property (nonatomic, copy) NSString *bg_color;
+
+@property (nonatomic, assign) BOOL is_default;
+//0：未被删除，1：已删除，2：彻底删除
+@property (nonatomic, assign) BOOL is_delete;
+
+@property (nonatomic, assign) NSInteger priority;
+
+
+
+@property (nonatomic, assign) NSInteger count;
+@property (nonatomic, assign) NSInteger length;
+
+@end
